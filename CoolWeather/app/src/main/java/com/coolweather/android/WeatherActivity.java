e17cd39dc180df1e71bdb5385656a731319fc2c5
huangchengdu <huang2009303513@gmail.com>
2019-04-11 23:50:00 +0800

    左侧滑动切换天气地址。drawerLayout的使用。

--- CoolWeather/app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ CoolWeather/app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -4,11 +4,14 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -30,6 +33,10 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+
+    public Button navButton;
 
     public SwipeRefreshLayout swipeRefresh;
 
@@ -72,6 +79,15 @@ public class WeatherActivity extends AppCompatActivity {
         setContentView(R.layout.activity_weather);
 
         bingPicImg = findViewById(R.id.bing_pic_img);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navButton = findViewById(R.id.nav_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
         //初始化各控件
         weatherLayout = findViewById(R.id.weather_layout);
